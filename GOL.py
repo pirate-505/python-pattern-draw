9eaf51cfa5b489ffacf7b8ea5f5f7dec66da16dc
pirate-505 <piratem505@gmail.com>
2020-02-16 15:00:16 +0200

    Refactored some code and added some cool features
    - variable names are now little bit more readable
    - added methods for saving image as png
    - patterner thing now works with n*m matrices, not only square ones
    - changed ruleset format for more fun
    - press LMB in GOL to pause/resume or RMB to save image as png

--- GOL.py
+++ GOL.py
@@ -1,12 +1,13 @@
 import patterner as pt
-from random import randint
+from random import random
 import time
 
-#create randomized 50*50 matrix
-field = pt.rnd_matrix(50)
+pause = False
+# create randomized 60*60 matrix
+field = pt.rnd_matrix(60)
 
 '''
-#create planer for classic GOL rule
+# create planer for classic GOL rule
 field = pt.filled_matrix(50, 0)
 field[9][8] = 1
 field[9][9] = 1
@@ -17,66 +18,87 @@ field[7][7] = 1
 p = pt.Patterner(field, 10)
 l = len(field)
 
-#'pseudolife' ruleset
-ALIVE = [2, 3, 8]
-BORN = [3, 5, 7]
+# 'maze' ruleset
+# Format is "number of cells: probability".
+# The "probability" thing adds some random
+# Not really a finite-state machine now, just added this for fun.
+# To make it behave as normal Game of Life, make all the "probabilities" equal to 1
+alive_conditions = {'1': 0.95, '2': 0.95, '3': 0.95, '4': 0.95, '5': 0.95}
+born_conditions = {'3': 0.3}
 
 #'maze' ruleset
-#alive = [1, 2, 3, 4, 5]
-#born = [3]
+# alive = [1, 2, 3, 4, 5]
+# born = [3]
 
 print("%s x %s field generated" % (len(field), len(field[0])))
 
-def tor(c):
-	#toroidal closed surface
-	if (c < 0):
-		return c+l
-	else:
-		return c%l
 
-def gol(y, near):
-	#cell birth condition
-	if (y == 0 and near in BORN):
-		return 1
-	#cell survives condition
-	if (y == 1 and near in ALIVE):
-		return 1
-	#else die
-	return 0
+def tor(c):
+    '''Toroidal closed surface'''
+    return c + l if c < 0 else c % l
+
+
+def gol_step(y, near):
+    # cell birth condition
+    if (y == 0 and str(near) in born_conditions):
+        return 1 if born_conditions[str(near)] > random() else 0
+    # cell survives condition
+    if (y == 1 and str(near) in alive_conditions):
+        return 1 if alive_conditions[str(near)] > random() else 0
+    # else die
+    return 0
+
 
 def sum_near(m, i, j):
-	#calculate the sum of all neighbour cells
-	s = 0
-	for ii in range(-1,2):
-		for jj in range(-1,2):
-			s += m[tor(i+ii)][tor(j+jj)]
-	s -= m[i][j]
-	return s
+    '''Calculate the sum of all neighbour cells'''
+    s = 0
+    for ii in range(-1, 2):
+        for jj in range(-1, 2):
+            s += m[tor(i + ii)][tor(j + jj)]
+    s -= m[i][j]
+    return s
+
 
 def iterate(m):
-	#new state matrix
-	f2 = [row[:] for row in m]
-	for i in range(l):
-		for j in range(l):
-			near = sum_near(m, i, j)
-			#some debug stuff
-			#if(m[i][j] != 0):
-			#	print('i = %d, j = %d; cell = %d, nearsum = %d' % (i,j, m[tor(i)][tor(j)], near))
-			f2[tor(i)][tor(j)] = gol(m[tor(i)][tor(j)], near)
-	return f2
+    # New state matrix
+    f2 = [row[:] for row in m]
+    for i in range(l):
+        for j in range(l):
+            near = sum_near(m, i, j)
+            f2[tor(i)][tor(j)] = gol_step(m[tor(i)][tor(j)], near)
+    return f2
+
+
+def key1_callback(event):
+    global pause
+    pause = not pause
+    print("pause: ", pause)
+
+
+def key3_callback(event):
+    print("Saved as ", p.save_image())
+
+
+p.bind("<Button-1>", key1_callback)  # press LMB to pause/resume
+p.bind("<Button-3>", key3_callback)  # press RMB to save current state as png image
 
 iteration = 0
+time_counter = time.time()
 
+# basic main loop
 while (True):
-	p.clean()
-	p.set_matrix(field)
-	p.draw()
-	#print(pt.text_draw(field))
-	time.sleep(0.1)
-	field = iterate(field)
-	iteration += 1
-	print('Iteration %d' % iteration)
-	#if(field == iterate(field)):
-	#	print('*** Stable state! ***')
+    p.tk_update()
+    if pause:
+        time.sleep(0.05)
+        continue
+    if time.time() - time_counter >= 0.1:
+        time_counter = time.time()
+        p.clean()
+        p.set_matrix(field)
+        p.draw()
+        field = iterate(field)
+        iteration += 1
+        print('Iteration %d' % iteration)
+        time.sleep(0.05)
 
 input()
--- patterner.py
+++ patterner.py
@@ -2,110 +2,118 @@
 
 from tkinter import *
 from random import randint, choice
+from datetime import datetime
+from PIL import Image
+import io
+
 
 class Patterner:
-	def __init__(self, matrix, pixel_size = 1, mode = 'wb'):
-		'''matrix - an array or tuple of rows of elements
-		mode - "wb" for white/black, "color" for color
-		pixel_size - size of pseudopixel 
-        In color mode uses cell values from matrix as colors; 
-        color could be in any tk-acceptable format
-        !!!currently works only with square matrixes'''
-		self.p_size = pixel_size
-		self.mode = mode
-		self.tk = Tk()
-		self.set_matrix(matrix)
-		self.create_canvas(self.c_size)
+    def __init__(self, matrix, pixel_size=1, mode="wb"):
+        '''matrix - an array or tuple of rows of elements
+        mode - "wb" for white/black, "color" for color, default - "wb"
+        pixel_size - size of pseudopixel
+        In color mode it uses cell values from matrix as colors;
+        Color could be in any tk-acceptable format'''
+        self.pixel_size = pixel_size
+        self.mode = mode
+        self.tk = Tk()
+        self.set_matrix(matrix)
+        self.canvas = Canvas(self.tk, width=self.canvas_width, height=self.canvas_height)
+        self.canvas.pack()
 
-	def set_matrix(self, m):
-		'''A method to change matrix'''
-		self.matrix = m
-		self.c_size = len(self.matrix) * self.p_size + 1
-		self.rng = range(0, len(self.matrix))
+    def set_matrix(self, matrix):
+        self.matrix = matrix
+        self.matrix_width = len(self.matrix[0])
+        self.matrix_height = len(self.matrix)
 
-	def set_mode(self, mode):
-		'''changes mode'''
-		self.mode = mode
+        self.canvas_width = self.matrix_width * self.pixel_size + 1
+        self.canvas_height = self.matrix_height * self.pixel_size + 1
 
-	def clean(self):
-		'''cleans canvas'''
-		self.c.delete('all')
-		
-	def create_canvas(self, size):
-		self.c = Canvas(self.tk, width=size, height=size)
-		self.c.pack()
+    def set_mode(self, mode):
+        '''mode - "wb" for white/black, "color" for color'''
+        self.mode = mode
 
-	def draw(self):
-		'''draw method'''
-		x0, y0, x1, y1 = 0, 0, self.p_size, self.p_size
+    def clean(self):
+        self.canvas.delete("all")
 
-		for i in self.rng:
-			for ii in self.rng:
-				if (self.mode == 'color'):
-					self.c.create_rectangle(x0, y0, x1, y1, fill = self.matrix[i][ii], width = 0)
-				else:
-					if (self.matrix[i][ii] >= 1):
-						self.c.create_rectangle(x0, y0, x1, y1, fill = 'black', width = 0)
+    def draw(self):
+        x0, y0, x1, y1 = 0, 0, self.pixel_size, self.pixel_size
 
-				x0 += self.p_size
-				x1 += self.p_size
-			x0 = 0
-			y0 += self.p_size
-			x1 = self.p_size
-			y1 += self.p_size
-		self.tk.update_idletasks()
-		self.tk.update()
-		
+        for i in range(0, self.matrix_height):
+            for ii in range(0, self.matrix_width):
+                if (self.mode == 'color'):
+                    self.canvas.create_rectangle(
+                        x0, y0, x1, y1, fill=self.matrix[i][ii], width=0)
+                else:
+                    if (self.matrix[i][ii] >= 1):
+                        self.canvas.create_rectangle(
+                            x0, y0, x1, y1, fill="black", width=0)
 
-	def bind(self, key, event):
-		self.tk.bind(key, event)
+                x0 += self.pixel_size
+                x1 += self.pixel_size
+            x0 = 0
+            y0 += self.pixel_size
+            x1 = self.pixel_size
+            y1 += self.pixel_size
+        self.tk_update()
+
+    def bind(self, key, event):
+        self.tk.bind(key, event)
+
+    def tk_update(self):
+        self.tk.update_idletasks()
+        self.tk.update()
+
+    def _generate_filename(self):
+        return datetime.strftime(datetime.today(), "%d%m%y_%H%M%S")
+
+    def save_image(self, filename=None):
+        fn = filename if filename else self._generate_filename()
+        ps = self.canvas.postscript(colormode="color")
+        img = Image.open(io.BytesIO(ps.encode("utf-8")))
+        img.save(fn + ".png", "png")
+        return fn + ".png"
+
 
 def rnd_matrix(n):
-	'''Returns matrix with size n*n filled with 1s or 0s'''
-	return [[randint(0,1) for x in range(n)] for xx in range(n)]
+    '''Returns n*n matrix filled with 1s or 0s'''
+    return [[randint(0, 1) for x in range(n)] for xx in range(n)]
+
 
 def rnd_color():
-	'''Pretty original way to create random color in hex.
+    '''Pretty original way to create random color in hex.
     Returns color string'''
-	ch = ['a','b','c','d','e','f']
-	clr = '#'
-	for i in range(6):
-		x = randint(0,100)
-		if(x < 50):
-			clr += choice(ch)
-		else:
-			clr += str(randint(0,9))
-	return clr
+    ch = ['a', 'b', 'c', 'd', 'e', 'f']
+    clr = '#'
+    for i in range(6):
+        x = randint(0, 100)
+        if(x < 50):
+            clr += choice(ch)
+        else:
+            clr += str(randint(0, 9))
+    return clr
+
 
 def rnd_matrix_c(n):
-	'''returns matrix with size n*n and filled with random colors'''
-	return [[rnd_color() for x in range(n)] for xx in range(n)]
+    '''returns n*n-sized matrix filled with random colors'''
+    return [[rnd_color() for x in range(n)] for xx in range(n)]
+
 
 def filled_matrix(n, fill):
-	'''returns matrix with size n*n and filled with "fill"'''
-	return [[fill for i in range(n)] for j in range(n)] 
+    '''returns n*n-sized matrix filled with "fill"'''
+    return [[fill for i in range(n)] for j in range(n)]
 
-def text_draw(m):
-	'''An alternate simple way to draw matrix filled with same length elements - just prints it out!'''
-	l = len(m)
-	s = ''
-	for i in range(l):
-		for j in range(l):
-			s += str(m[i][j]) + ' '
-		s += '\n'
-	return s
 
-if __name__ == '__main__':
-	# Example of usage
-	import time
-	m = rnd_matrix_c(20)
-	p = Patterner(m, 20, 'color')
-	print(p)
-	p.draw()
-	time.sleep(2)
-	p.clean()
-	p.set_mode('wb')
-	p.set_matrix(rnd_matrix(20))
-	p.draw()
-	
-	input()
+if __name__ == "__main__":
+    # Example of usage
+    import time
+    m = rnd_matrix_c(20)
+    p = Patterner(m, 30, "color")
+    p.draw()
+    time.sleep(2)
+    p.clean()
+    p.set_mode("wb")
+    p.set_matrix(rnd_matrix(20))
+    p.draw()
+
+    input()
